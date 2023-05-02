8deffc80298c4bb843b12f32c73f015d12e984c7
vhaguiar <vhaguiar@gmail.com>
2023-05-02 08:36:59 -0500

    k means and k modes
    k means and k modes

---
+++ NN/6_Clustering.jl
@@ -0,0 +1,257 @@
+using Pkg
+# Create and activate a new environment
+Pkg.activate("Clustering_env")
+
+Pkg.add(["DataFrames", "Distances", "Clustering", "Random", "CategoricalArrays"])
+
+using DataFrames, Distances, Clustering, Random, CategoricalArrays
+
+##k means 
+# make a random dataset with 1000 random 5-dimensional points
+X = rand(5, 1000)
+
+# cluster X into 20 clusters using K-means
+R = kmeans(X, 20; maxiter=200, display=:iter)
+
+@assert nclusters(R) == 20 # verify the number of clusters
+
+a = assignments(R) # get the assignments of points to clusters
+c = counts(R) # get the cluster sizes
+M = R.centers # get the cluster centers
+
+### k-modes
+using DataFrames, Clustering, Distances
+
+using DataFrames, Random
+
+function generate_choices(consumers, ranking_map, choice_sets)
+    choices = []
+    for i in 1:length(consumers)
+        ranking = ranking_map[rankings[i]] # Use the ranking_map dictionary instead of rankings
+        choice_set = choice_sets[i]
+        choice = choice_set[argmax([findfirst(isequal(x), ranking) for x in choice_set])]
+        push!(choices, choice)
+    end
+    return choices
+end
+
+
+
+scale=100
+n = 120*scale
+consumers = 1:n
+choice_sets = vcat(fill(["a"], 60*scale), fill(["b", "c"], 60*scale))
+rankings = vcat(fill(1, 10*scale), fill(2, 15*scale), fill(3, 20*scale), fill(4, 30*scale), fill(5, 35*scale), fill(6, 10*scale))
+
+ranking_map = Dict(
+    1 => ["a", "b", "c"],
+    2 => ["a", "c", "b"],
+    3 => ["b", "a", "c"],
+    4 => ["b", "c", "a"],
+    5 => ["c", "a", "b"],
+    6 => ["c", "b", "a"]
+)
+
+# Shuffle the consumers, choice_sets, and rankings arrays
+Random.seed!(42)
+shuffled_indices=shuffle(consumers) 
+shuffled_indices_1 = shuffle(consumers)
+shuffled_indices_2 = shuffle(consumers)
+shuffled_indices_3 = shuffle(consumers)
+shuffled_choice_sets_1 = choice_sets[shuffled_indices_1]
+shuffled_choice_sets_2 = choice_sets[shuffled_indices_2]
+shuffled_choice_sets_3 = choice_sets[shuffled_indices_3]
+
+
+shuffled_rankings = rankings[shuffled_indices]
+
+# Generate choices for each time period
+choices_t1 = generate_choices(consumers, ranking_map, shuffled_choice_sets_1)
+choices_t2 = generate_choices(consumers, ranking_map, shuffled_choice_sets_2)
+choices_t3 = generate_choices(consumers, ranking_map, shuffled_choice_sets_3)
+
+# Create the DataFrame
+data = DataFrame(
+    consumer_id = consumers,
+    choice_t1 = categorical(choices_t1),
+    choice_t2 = categorical(choices_t2),
+    choice_t3 = categorical(choices_t3)
+)
+
+data_matrix = Matrix(data[:, 2:end])
+
+
+
+# Define the custom K-modes distance function
+function k_modes_distance(a::AbstractVector, b::AbstractVector)
+    return sum([count(a[i] .!= b[i]) for i in 1:length(a)])
+end
+
+##
+# # Create a dictionary to map categorical values to integers
+# # Create a dictionary to map categorical values to integers
+# mapping = Dict(
+#     "a" => 1, "b" => 2, "c" => 3
+# )
+
+# # Convert data_matrix to numeric matrix using the mapping
+# numeric_data_matrix = Matrix{Int}(undef, size(data_matrix))
+# for i in 1:size(data_matrix, 1)
+#     for j in 1:size(data_matrix, 2)
+#         numeric_data_matrix[i, j] = mapping[data_matrix[i, j]]
+#     end
+# end
+
+# # Transpose the numeric_data_matrix
+# transposed_data_matrix = numeric_data_matrix'
+
+
+# Compute the pairwise distance matrix using the custom function
+n = size(data_matrix, 1)
+distances = Matrix{Int}(undef, n, n)
+
+# Compute the distance matrix using the custom function
+for i in 1:n
+    for j in 1:n
+        distances[i, j] = k_modes_distance(data_matrix[i, :], data_matrix[j, :])
+    end
+end
+
+k = 2^3
+result = kmedoids(distances, k)
+
+data[!, "cluster"] = result.assignments
+
+println(result.assignments)
+
+
+
+using EvalMetrics
+
+function choice_set_to_label(choice_set)
+    if choice_set == ["a"]
+        return 2
+    elseif choice_set == ["b", "c"]
+        return 1
+    # elseif choice_set == ["a", "c"]
+    #     return 1
+    else
+        error("Invalid choice set")
+    end
+end
+
+choice_set_labels_1 = [choice_set_to_label(choice_set) for choice_set in choice_sets[shuffled_indices_1]]
+choice_set_labels_2 = [choice_set_to_label(choice_set) for choice_set in choice_sets[shuffled_indices_2]]
+choice_set_labels_3 = [choice_set_to_label(choice_set) for choice_set in choice_sets[shuffled_indices_3]]
+
+choice_set_labels=choice_set_labels_1+ choice_set_labels_2*10+choice_set_labels_3*100
+
+choice_set_labels
+data[!, "truelabels"]=choice_set_labels 
+
+##validation  111
+cluster_111_rows = data[data[!, :truelabels] .== 111, :]
+unique(cluster_111_rows.cluster)
+
+##validation 221
+cluster_221_rows = data[data[!, :truelabels] .== 221, :]
+unique(cluster_221_rows.cluster)
+
+##validation 222
+cluster_222_rows = data[data[!, :truelabels] .== 222, :]
+unique(cluster_222_rows.cluster)
+
+##validation 211
+cluster_211_rows = data[data[!, :truelabels] .== 211, :]
+unique(cluster_211_rows.cluster)
+
+##validation 121
+cluster_121_rows = data[data[!, :truelabels] .== 121, :]
+unique(cluster_121_rows.cluster)
+
+##validation 122
+cluster_122_rows = data[data[!, :truelabels] .== 122, :]
+unique(cluster_122_rows.cluster)
+
+##validation 212
+cluster_212_rows = data[data[!, :truelabels] .== 212, :]
+unique(cluster_212_rows.cluster)
+
+##validation 112
+cluster_112_rows = data[data[!, :truelabels] .== 112, :]
+unique(cluster_112_rows.cluster)
+
+
+data.cluster
+# Create the confusion matrix
+function create_confusion_matrix(true_labels, predicted_labels)
+    n_labels = k
+    confusion_matrix = zeros(Int, n_labels, n_labels)
+    
+    for (true_label, predicted_label) in zip(true_labels, predicted_labels)
+        confusion_matrix[true_label, predicted_label] += 1
+    end
+    
+    return confusion_matrix
+end
+
+confusion_matrix = create_confusion_matrix(choice_set_labels, data.cluster)
+
+
+
+using Combinatorics
+
+# Define the set of values for v01, v02, and v03
+values = [1, 2]
+
+# Generate all possible combinations of v01, v02, and v03
+combinations = collect(Iterators.product(values, values, values))
+
+# Initialize an empty dictionary
+mapping = Dict{Int, Int}()
+
+# Iterate over the combinations and create the mapping for v1
+# Create a dictionary for mapping
+mapping = Dict(
+    111 => 1,
+    112 => 2,
+    121 => 3,
+    122 => 4,
+    211 => 5,
+    212 => 6,
+    221 => 7,
+    222 => 8
+)
+
+
+
+# Print the resulting mapping
+println(mapping)
+
+mapping[choice_set_labels[1]]
+
+# Define the choice_set_labels vector with some example values
+
+# Initialize an empty vector to store the mapped values
+mapped_choice_set_labels = Int[]
+
+# Iterate over the choice_set_labels vector and apply the mapping
+for label in choice_set_labels
+    mapped_label = mapping[label]
+    push!(mapped_choice_set_labels, mapped_label)
+end
+
+# Print the resulting mapped_choice_set_labels vector
+println(mapped_choice_set_labels)
+println(data.cluster)
+
+relabel=Dict(
+    4=>6,
+    7=>1,
+    8=>7,
+    6=>4,
+    5=>5,
+    3=>3,
+    2=>2,
+    1=>8
+)
